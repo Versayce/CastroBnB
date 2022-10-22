6c451fe341522d96a74331699de95ca206708901
Alex <acastro166@gmail.com>
2022-10-21 21:43:12 -0700

    added user logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -28,7 +28,16 @@ router.post(
         user
       });
     }
-  );
+);
+
+// Log out
+router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+);
 
 
 module.exports = router;
