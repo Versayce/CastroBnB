989805a5483823388b30e635a8a508d8d69a1ab3
Alex <acastro166@gmail.com>
2022-10-25 13:23:09 -0700

    removed unessecary code from spots.js router file (Users model import)

--- backend/db/migrations/20221024231031-create-spots.js
+++ backend/db/migrations/20221024231031-create-spots.js
@@ -52,6 +52,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('spots');
+    await queryInterface.dropTable('Spots');
   }
 };
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { Spot } = require('../../db/models');
-const { User } = require('../../db/models');
+
 const router = express.Router();
 
 
