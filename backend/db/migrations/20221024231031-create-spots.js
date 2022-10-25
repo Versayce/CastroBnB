4be89ea9e0a53f735e81857c17fc70a24464ab47
Alex <acastro166@gmail.com>
2022-10-25 13:02:03 -0700

    edited name of spots migration

--- backend/db/migrations/20221024231031-create-spots.js
+++ backend/db/migrations/20221024231031-create-spots.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('spots', {
+    await queryInterface.createTable('Spots', {
       id: {
         allowNull: false,
         autoIncrement: true,
