58b04f6d6f3f45d14d481c3f3e9b0a5ec4b379c4
Alex <acastro166@gmail.com>
2022-10-26 12:46:16 -0700

    added associations to spotimages model and spot model

--- backend/db/models/spotimage.js
+++ backend/db/models/spotimage.js
@@ -11,9 +11,14 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      SpotImage.belongsTo(models.Spot, {foreignKey: 'spotId'})
     }
   }
   SpotImage.init({
+    spotId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
     url: DataTypes.STRING,
     preview: DataTypes.BOOLEAN
   }, {
