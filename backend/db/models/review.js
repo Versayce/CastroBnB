b945c0880c2a15e93d49211599f39e527378a55a
Alex <acastro166@gmail.com>
2022-10-26 09:10:20 -0700

    created new model and migration for reviews

---
+++ backend/db/models/review.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class review extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  review.init({
+    review: DataTypes.STRING,
+    stars: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'review',
+  });
+  return review;
+};
