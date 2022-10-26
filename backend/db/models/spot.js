7650e73fa23f17f742adb3479e9aed1c796c850d
Alex <acastro166@gmail.com>
2022-10-26 13:06:34 -0700

    created bookings model and migration, added associations to all related models, added foreign keys to model and migration files

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Spot.hasMany(models.SpotImage, {foreignKey: 'spotId'})
-      //Spot.hasMany(models.Booking, {foreignKey: 'spotId'})
+      Spot.hasMany(models.Booking, {foreignKey: 'spotId'})
       Spot.hasMany(models.Review, {foreignKey: 'spotId'});
       Spot.belongsTo(models.User, {foreignKey: 'ownerId'});
     }
