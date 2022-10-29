437d0f6ab259525a644a74edd7320ee1c0dbadd8
Alex <acastro166@gmail.com>
2022-10-29 10:02:30 -0700

    completed 99% of get current users bookings. Still needs previewImage

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -1,11 +1,26 @@
 const express = require('express')
-const { Review, User, Spot, ReviewImage, SpotImage } = require('../../db/models');
+const { Review, User, Spot, ReviewImage, SpotImage, Booking } = require('../../db/models');
 const sequelize = require('sequelize');
 const { requireAuth } = require('../../utils/auth');
 const { Model } = require('sequelize');
 
 const router = express.Router();
 
-
+router.get('/current', requireAuth, async (req, res) => {
+    const Bookings = await Booking.findAll({
+        where: {
+            userId: req.user.id,
+        },
+        include: {
+            model: Spot,
+            attributes: {
+                exclude: ['createdAt', 'updatedAt', 'description']
+            }
+        }
+    })
+    return res.json(
+        {Bookings}
+    )
+})
 
 module.exports = router;
