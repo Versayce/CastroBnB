ffd7b9dc9de5ba84b104236061b90a3d4ba8da3b
Alex <acastro166@gmail.com>
2022-11-14 23:22:52 -0800

    added styling to spot cards

--- frontend/src/components/Spots/SpotCard.js
+++ frontend/src/components/Spots/SpotCard.js
@@ -4,13 +4,21 @@ export default function SpotCard({ spots }){
     //console.log('spots: ', spots)
     return (
         spots.map((spot) => (
-            <div key={spot.id} className="spot-card">
-                <img src={spot.previewImage} className="spot-image" />
-                <h4>{`${spot.city}, ${spot.state}`}</h4>
-                <p>{spot.name}</p>
-                <p>{`$${spot.price} night`}</p>
-                <p>{`AVG Rating: ${spot.avgRating}`}</p>
-            </div>
+            <>
+                <div key={spot.id} className="spot-card">
+                    <img src={spot.previewImage} className="spot-image" />
+                    <div className='spot-info-container'>
+                        <div className='spot-info'>
+                            <p style={{fontWeight: 'bold'}}>{`${spot.city}, ${spot.state}`}</p>
+                            <p>{spot.name}</p>
+                            <p>{`$${spot.price} night`}</p>
+                        </div>
+                        <div className='spot-rating'>
+                            <p>{`AVG Rating: ${spot.avgRating}`}</p>
+                        </div>
+                    </div>
+                </div>
+            </>
         ))
     )
 }
