65fec50e0b0bd5143bfb8072396c0394994cfea7
Alex <acastro166@gmail.com>
2022-11-15 12:57:32 -0800

    Testing routing for get one spot

--- frontend/src/components/Spots/SpotCard.js
+++ frontend/src/components/Spots/SpotCard.js
@@ -1,11 +1,21 @@
 import './Spots.css'
-import { useNavigate } from "react-router-dom";
+import { useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { getOneSpot } from '../../store/spots';
+
 
 export default function SpotCard({ spots }){
+    let history = useHistory();
+    const spot = useSelector(state => state.spots.oneSpot)
+    console.log('spot: ', spot)
+
+    const clickHandler = () => {
+        history.push(`/spots/1`)
+    }
     //console.log('spots: ', spots)
     return (
         spots.map((spot) => (
-            <div key={spot.id} className="spot-card">
+            <div key={spot.id} className="spot-card" onClick={clickHandler}>
                 <img src={spot.previewImage} className="spot-image" />
                 <div className='spot-info-container'>
                     <div className='spot-info'>
