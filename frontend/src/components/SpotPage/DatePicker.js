225ae5c062437d1c3a2ba5659c25b222e783a935
Alex <acastro166@gmail.com>
2023-02-28 15:17:12 -0700

    added submit button

--- frontend/src/components/SpotPage/DatePicker.js
+++ frontend/src/components/SpotPage/DatePicker.js
@@ -3,28 +3,35 @@ import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
 
 import { DateRangePicker } from 'react-dates';
+// import e from 'express';
 
 const DatePicker = () => {
     const [startDate, setStartDate] = useState(null)
     const [endDate, setEndDate] = useState(null)
     const [state, setState]= useState({
-        // startDate: null,
-        // endDate: null,
         focusedInput: null,
     });
 
-    console.log('CURRENT STATE IN DATEPICKER', state)
+    console.log('START DATE: ', startDate?.toDate(), '\n', 'END DATE: ', endDate?.toDate())
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+    }
 
     return (
-        <DateRangePicker
-            startDateId="startDate"
-            endDateId="endDate"
-            startDate={startDate}
-            endDate={endDate}
-            onDatesChange={({ startDate, endDate }) => (setStartDate(startDate), setEndDate(endDate))}
-            focusedInput={state.focusedInput}
-            onFocusChange={(focusedInput) => {setState({ focusedInput })}}
-        />
+        <>
+            <DateRangePicker
+                startDateId="startDate"
+                endDateId="endDate"
+                startDate={startDate}
+                endDate={endDate}
+                onDatesChange={({ startDate, endDate }) => (setStartDate(startDate), setEndDate(endDate))}
+                focusedInput={state.focusedInput}
+                onFocusChange={(focusedInput) => {setState({ focusedInput })}}
+            />
+            <button onClick={(e) => handleSubmit}>Submit</button>
+        </>
     )
 }
 
