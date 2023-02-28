059f4d9cb506c343d158f687ed1b188815c5b54f
Alex <acastro166@gmail.com>
2023-02-28 13:43:22 -0700

    created more useStates for datepicker

--- frontend/src/components/SpotPage/DatePicker.js
+++ frontend/src/components/SpotPage/DatePicker.js
@@ -5,9 +5,11 @@ import 'react-dates/lib/css/_datepicker.css';
 import { DateRangePicker } from 'react-dates';
 
 const DatePicker = () => {
+    const [startDate, setStartDate] = useState(null)
+    const [endDate, setEndDate] = useState(null)
     const [state, setState]= useState({
-        startDate: null,
-        endDate: null,
+        // startDate: null,
+        // endDate: null,
         focusedInput: null,
     });
 
@@ -17,9 +19,9 @@ const DatePicker = () => {
         <DateRangePicker
             startDateId="startDate"
             endDateId="endDate"
-            startDate={state.startDate}
-            endDate={state.endDate}
-            onDatesChange={({ startDate, endDate }) => {setState({ startDate, endDate })}}
+            startDate={startDate}
+            endDate={endDate}
+            onDatesChange={({ startDate, endDate }) => (setStartDate(startDate), setEndDate(endDate))}
             focusedInput={state.focusedInput}
             onFocusChange={(focusedInput) => {setState({ focusedInput })}}
         />
